8f2ec355842edc17a1cf4d89d8ef5657a2c02ec9
Yovan Rodriguez <yovanr129@gmail.com>
2026-02-01 17:37:50 -0700

    Se añadieron validaciones a los metodos de posts

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -2,34 +2,49 @@
 
     <h1>Aqui se mostrara el formulario</h1>
 
+    @if ($errors->any())
+    
+        <div>
+            <h2>Errores:</h2>
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>
+                        {{$error}}
+                    </li>
+                @endforeach
+            </ul>
+        </div>
+
+    @endif
+
     <form action="{{route('posts.update', $post)}}" method="POST">
         @csrf
         @method('PUT')
 
         <label>
             Titulo:
-            <input type="text" name="title" value="{{$post->title}}">
+            <input type="text" name="title" value="{{old('title', $post->title)}}">
         </label>
 
         <br>
 
         <label>
             Slug:
-            <input type="text" name="slug" value="{{$post->slug}}">
+            <input type="text" name="slug" value="{{old('slug', $post->slug)}}">
         </label>
 
         <br>
 
         <label>
             Categoria:
-            <input type="text" name="category" value="{{$post->category}}">
+            <input type="text" name="category" value="{{old('category', $post->category)}}">
         </label>
 
         <br>
 
         <label>
             Contenido: 
-            <textarea name="content" >{{$post->content}}</textarea>
+            <textarea name="content" >{{old('content', $post->content)}}</textarea>
         </label>
 
         <br>
